1fce44f3794fc4a44a4f0f36dd9822e66289b4ec
SilmarNascimento <silmarfnascimento@gmail.com>
2025-01-03 08:12:57 -0300

    refactor: altera a estrutura das pastas, aadiciona plugin do protobuf com versões compatíveis com as dependências

--- gRPCClient/src/main/java/org/example/Main.java
+++ gRPCClient/src/main/java/org/example/Main.java
@@ -1,18 +1,41 @@
-package org.example;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-
-  public static void main(String[] args) {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    System.out.printf("Hello and welcome!");
-
-    for (int i = 1; i <= 5; i++) {
-      //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-      // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-      System.out.println("i = " + i);
-    }
-  }
-}
+//package org.example;
+//
+//import io.grpc.ManagedChannel;
+//import io.grpc.ManagedChannelBuilder;
+//import proto.GreeterGrpc;
+//import proto.GRPC;
+//
+//public class Main {
+//  public static void main(String[] args) {
+//    /* *
+//     * Establish a connection to the server using the class ManagedChannelBuilder and the function usePlaintext().
+//     * The function usePlainText() should only be used for testing or for APIs where the use of such API or the data
+//     * exchanged is not sensitive.
+//     * */
+//    ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8999).usePlaintext().build();
+//
+//    /* *
+//     * A blocking-style stub instance of Greeter service. We can have two types of stubs: blocking and async.
+//     * Blocking stubs are synchronous. Non-blocking stubs are asynchronous.
+//     * Take care if you want to call an RPC function on a blocking stub from UI thread
+//     * (cause an unresponsive/laggy UI).
+//     * */
+//    GreeterGrpc.GreeterBlockingStub bookStub = GreeterGrpc.newBlockingStub(channel);
+//
+//    /* *
+//     * An asynchronous instance of the above declaration.
+//     * GreeterGrpc.GreeterStub bookStub = GreeterGrpc.newStub(channel);
+//     * */
+//
+//    /* *
+//     * We can now use the gRPC function via our instance of GreeterBlockingStub bookStub.
+//     * Below we call the function sayHello(Helloworld.HelloRequest request) with name field value set to "gRPC".
+//     * This function returns a value of type  Helloworld.HelloReply that is saved in our instance reply.
+//     * We can get via generated functions every field from our message, in this example, we have just one field.
+//     * */
+//    Helloworld.HelloReply reply = bookStub.sayHello(Helloworld.HelloRequest.newBuilder().setName("gRPC").build());
+//    System.out.println(reply.getMessage());
+//
+//    channel.shutdown();
+//  }
+//}
