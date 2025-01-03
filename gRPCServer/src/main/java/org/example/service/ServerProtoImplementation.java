73c3f750d6bddc254234f89d510d52d81bdaaac7
SilmarNascimento <silmarfnascimento@gmail.com>
2025-01-03 10:13:22 -0300

    feat: implementa a classe de implementação da interface de gRPC e implementa o código da Main configurando o servidor

--- gRPCServer/src/main/java/org/example/service/ServerProtoImplementation.java
+++ gRPCServer/src/main/java/org/example/service/ServerProtoImplementation.java
@@ -1,21 +1,22 @@
-package service;
+package org.example.service;
 
 import io.grpc.stub.StreamObserver;
+import proto.GRPC.CommunicationRequest;
+import proto.GRPC.CommunicationReply;
 import proto.GreeterGrpc;
-import proto.GRPC;
 
-public class GreeterImpl extends GreeterGrpc.GreeterImplBase {
+public class ServerProtoImplementation extends GreeterGrpc.GreeterImplBase {
   /*
    * We observe here that some words have an @, these are Annotations.
    * Annotations are used to provide supplement information about a program. We can autogenerate this functions,
    * in Intellij we can use the shortcut ctrl + O to do this.
    * */
   @Override
-  public void sayHello(GRPC.CommunicationRequest request, StreamObserver<GRPC.CommunicationReply> responseObserver) {
+  public void sayHello(CommunicationRequest request, StreamObserver<CommunicationReply> responseObserver) {
     /* Build a response composed with the constant Hello and the name variable from the request.
      * For example, if the request is: gRPC the reply will be: "Hello gRPC".
      */
-    GRPC.CommunicationReply reply = GRPC.CommunicationReply.newBuilder().setMessage("Hello " + request.getName()).build();
+    CommunicationReply reply = CommunicationReply.newBuilder().setMessage("Hello " + request.getName()).build();
     responseObserver.onNext(reply);
     /* We use the response observer's onCompleted method to specify that we've finished dealing with the RPC */
     responseObserver.onCompleted();
